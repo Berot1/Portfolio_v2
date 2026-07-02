5c29d9f824653181dceaebb13440d1c25334b788
Gil Bernard F. Maglinte <127652117+Berot1@users.noreply.github.com>
2026-07-02 16:33:03 +0800

    change
    change

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import { personalInfo } from '@/data/portfolio';
 
 export default function Footer() {
   return (
-    <footer className="pt-8 border-t border-zinc-200 flex flex-col md:flex-row items-center justify-center gap-6">
+    <footer className="pt-8 flex flex-col md:flex-row items-center justify-center gap-6">
       <p className="text-zinc-500 text-sm">© {new Date().getFullYear()} {personalInfo.name}. All rights reserved.</p>
     </footer>
   );
